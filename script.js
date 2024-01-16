20f32115c8d431c5fecf490670aadf3a35d27cec
RAJKUMAR-35 <rajkumaranbu192@gmail.com>
2024-01-16 17:14:25 +0530

    Changes Updated

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/* ----- NAVIGATION BAR FUNCTION ----- */
+
     function myMenuFunction(){
       var menuBtn = document.getElementById("myNavMenu");
 
@@ -8,8 +8,6 @@
         menuBtn.className = "nav-menu";
       }
     }
-
-/* ----- ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING ----- */
     window.onscroll = function() {headerShadow()};
 
     function headerShadow() {
@@ -30,8 +28,6 @@
       }
     }
 
-
-/* ----- TYPING EFFECT ----- */
    var typingEffect = new Typed(".typedText",{
       strings : ["Rajkumar","MERN Developer"],
       loop : true,
@@ -40,8 +36,6 @@
       backDelay : 2000
    })
 
-
-/* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
    const sr = ScrollReveal({
           origin: 'top',
           distance: '80px',
@@ -49,7 +43,6 @@
           reset: true     
    })
 
-  /* -- HOME -- */
   sr.reveal('.featured-text-card',{})
   sr.reveal('.featured-name',{delay: 100})
   sr.reveal('.featured-text-info',{delay: 200})
@@ -57,16 +50,10 @@
   sr.reveal('.social_icons',{delay: 200})
   sr.reveal('.featured-image',{delay: 300})
   
-
-  /* -- PROJECT BOX -- */
   sr.reveal('.project-box',{interval: 200})
 
-  /* -- HEADINGS -- */
   sr.reveal('.top-header',{})
 
-/* ----- ## -- SCROLL REVEAL LEFT_RIGHT ANIMATION -- ## ----- */
-
-  /* -- ABOUT INFO & CONTACT INFO -- */
   const srLeft = ScrollReveal({
     origin: 'left',
     distance: '80px',
@@ -77,7 +64,6 @@
   srLeft.reveal('.about-info',{delay: 100})
   srLeft.reveal('.contact-info',{delay: 100})
 
-  /* -- ABOUT SKILLS & FORM BOX -- */
   const srRight = ScrollReveal({
     origin: 'right',
     distance: '80px',
@@ -87,11 +73,7 @@
   
   srRight.reveal('.skills-box',{delay: 100})
   srRight.reveal('.form-control',{delay: 100})
-  
-
-
-/* ----- CHANGE ACTIVE LINK ----- */
-  
+ 
   const sections = document.querySelectorAll('section[id]')
 
   function scrollActive() {
